6c69832dc0582495c3aed123c2ca224b3653f938
layomia <layomia@gmail.com>
2016-05-12 20:01:12 +0000

    Add actions and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,10 +27,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
    end
    
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesfully deleted"
+    redirect_to articles_path
+   end
+    
+   
    def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-        flash[:notice] = "Article was succesfully updated."
+        flash[:notice] = "Article was succesfully updated"
         redirect_to article_path(@article)
     else
         render 'edit'
